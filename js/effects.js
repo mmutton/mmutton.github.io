89345599b16abc0eb54f8385af1463600e31cb7d
Molly <mutton1@mac.com>
2018-01-30 12:35:51 -0800

    Changes

---
+++ js/effects.js
@@ -0,0 +1,16 @@
+
+
+
+$( document ).click(function() {
+  $( "#yourbutton" ).toggle( "explode" );
+});
+
+
+
+$( document ).click(function() {
+  $( "#mybutton" ).toggle( "explode" );
+});
+
+// $( document ).ready(function() {
+//   console.log( 'ready!' );
+// });
