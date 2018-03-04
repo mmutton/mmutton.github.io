766078845294ef756c5a6d2425bd9e7b6ac69cd9
Molly <mutton1@mac.com>
2018-03-04 13:19:02 -0800

    Video made full width Buttonz
    added jQuery solution to the video width so it would be wall to wall.  Changed the buttonz colors and the text

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -65,3 +65,17 @@ var onMapClickHandler = function(event) {
 // Enable map zooming with mouse scroll when the user clicks the map
 $('.map').on('click', onMapClickHandler);
 
+$('.jquery-background-video').bgVideo({fadeIn: 2000});
+$('.my-background-video').bgVideo({
+  fullScreen: false, // Sets the video to be fixed to the full window - your <video> and it's container should be direct descendents of the <body> tag
+  fadeIn: 500, // Milliseconds to fade video in/out (0 for no fade)
+  pauseAfter: 120, // Seconds to play before pausing (0 for forever)
+  fadeOnPause: false, // For all (including manual) pauses
+  fadeOnEnd: true, // When we've reached the pauseAfter time
+  showPausePlay: true, // Show pause/play button
+  pausePlayXPos: 'right', // left|right|center
+  pausePlayYPos: 'top', // top|bottom|center
+  pausePlayXOffset: '15px', // pixels or percent from side - ignored if positioned center
+  pausePlayYOffset: '15px' // pixels or percent from top/bottom - ignored if positioned center
+});
+
